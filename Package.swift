7c6e41a30b4fa4d044f4f1823161c86594a51132
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-03-27 17:30:44 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -29,11 +29,18 @@ let package = Package(
                 .package(path: "../MealzIOSFramework")
             ]
             )
+        } else if configurationMode == "devWithSPM" {
+            dependencies.append(contentsOf: [
+                .package(path: "../MealzUIModuleIOS"),
+                .package(path: "../MealzCore"),
+                .package(path: "../MealzIOSFrameworkSPM")
+            ]
+            )
         } else {
             dependencies.append(contentsOf: [
                 .package(url: "https://github.com/miamtech/releaseMealz", from: "1.0.0-beta3"),
-                .package(url: "https://github.com/miamtech/MealzIOSFrameworkSPM", exact: "1.0.0-beta2"),
-                .package(url: "https://github.com/miamtech/MealzUIModuleIOS", from: "1.0.2-beta1")
+                .package(url: "https://github.com/miamtech/MealzIOSFrameworkSPM", exact: "1.0.0-beta3"),
+                .package(url: "https://github.com/miamtech/MealzUIModuleIOS", from: "1.0.2-beta2")
             ]
             )
         }
@@ -48,6 +55,12 @@ let package = Package(
                     dependencies.append(contentsOf: [
                         .product(name: "MealzCore", package: "MealzCore"),
                         .product(name: "MealzIOSFramework", package: "MealzIOSFramework")
+                    ]
+                    )
+                } else if configurationMode == "devWithSPM" {
+                    dependencies.append(contentsOf: [
+                        .product(name: "MealzCore", package: "MealzCore"),
+                        .product(name: "MealzIOSFrameworkSPM", package: "MealzIOSFrameworkSPM")
                     ]
                     )
                 } else {
